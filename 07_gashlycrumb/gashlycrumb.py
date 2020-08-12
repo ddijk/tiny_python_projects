f9e3c0ce0784f28733a6d81a6c3d2962737b657d
ddijk <dick@dijkrosoft.nl>
2020-08-12 17:10:02 +0200

    My first solution to 07 gashlycrumb

---
+++ 07_gashlycrumb/gashlycrumb.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Author : dickdijk <dickdijk@localhost>
+Date   : 2020-08-12
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Gashlycrumb',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('letter',
+                        metavar='letter',
+                        nargs='+',
+                        help='Letter(s)')
+
+    parser.add_argument('-f',
+                        '--file',
+                        help='Input file',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        default='gashlycrumb.txt')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    file_arg = args.file
+    letter = args.letter
+
+    dict ={}
+    for line in file_arg:
+        dict[line[0]] = line.rstrip()
+
+    for c in letter:
+        print(f'{dict[c.upper()]}') if c.upper() in dict else print(f'I do not know "{c}".')
+
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
