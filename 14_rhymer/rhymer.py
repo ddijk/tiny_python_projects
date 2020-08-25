ea5a7205c9d25afe84fcbefc7ca5cbbd6df3f55a
ddijk <dick@dijkrosoft.nl>
2020-08-25 21:55:21 +0200

    fix stemmer

--- 14_rhymer/rhymer.py
+++ 14_rhymer/rhymer.py
@@ -9,6 +9,7 @@ import argparse
 import re
 import string
 
+
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
@@ -17,12 +18,10 @@ def get_args():
         description='Make rhyming "words"',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('word',
-                        metavar='word',
-                        help='A word to ryhme')
-
+    parser.add_argument('word', metavar='word', help='A word to ryhme')
 
     return parser.parse_args()
+
 
 def test_stemmer():
     """ Test stemmer """
@@ -32,14 +31,22 @@ def test_stemmer():
     assert stemmer('APPLE') == ('', 'apple')
     assert stemmer('RDNZL') == ('rdnzl', '')
 
+
 def stemmer(word):
-    return ('', '')
+    pattern = '([' + consonants() + ']+)?(.*)?'
+    (res1, rest2) = map(lambda x: x or '', re.match(pattern, word.lower()).groups())
+
+    return (res1, rest2)
+
 
 def consonants():
     return ''.join(filter(lambda c: c not in 'aeoui', string.ascii_lowercase))
 
+
 def test_consonants():
     assert consonants() == 'bcdfghjklmnpqrstvwxyz'
+
+
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
@@ -47,30 +54,31 @@ def main():
     args = get_args()
     text = args.word
 
-    pattern = '(['+consonants()+']+)?(.*)?'
+    pattern = '([' + consonants() + ']+)?(.*)?'
 
-    groups =  re.match(pattern, text.lower()).groups()
+    groups = re.match(pattern, text.lower()).groups()
 
     if groups[1]:
         printRhymeWords(groups[0], groups[1])
     else:
-        first=groups[0] or ''
+        first = groups[0] or ''
         print(f'Cannot rhyme "{args.word}"')
 
 
 def printRhymeWords(leading_consonants, rest):
 
-    replacements="""
+    replacements = """
     bl br ch cl cr dr fl fr gl gr pl pr sc sh sk sl sm sn sp st
 sw th tr tw thw wh wr sch scr shr sph spl spr squ str thr
   """.strip().split(" ")
-   
+
     replacements.extend(list(consonants()))
 
     sorted_list = sorted(replacements)
 
-    [print(c+rest) for c in sorted_list if c != leading_consonants]
-    
+    [print(c + rest) for c in sorted_list if c != leading_consonants]
+
+
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
