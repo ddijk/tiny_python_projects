e0c510f3acd18d9e5992f3585e00a99eb3c84ee9
ddijk <dick@dijkrosoft.nl>
2020-08-30 21:34:02 +0200

    fix pyling errors

--- 15_kentucky_friar/friar.py
+++ 15_kentucky_friar/friar.py
@@ -19,13 +19,9 @@ def get_args():
         description='Southern fry text',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('text',
-                        metavar='text',
-                        help='Input text or file')
+    parser.add_argument('text', metavar='text', help='Input text or file')
 
-    
-
-    args= parser.parse_args()
+    args = parser.parse_args()
 
     if os.path.isfile(args.text):
         lines = []
@@ -35,42 +31,46 @@ def get_args():
         args.text = '\n'.join(lines)
     return args
 
+
 def fry(word):
+    """ Convert some words to Kentucky slang"""
 
-    pattern1 = "([yY])(ou)([\W]["+string.ascii_letters+"]*)"
-    m = re.match(pattern1, word)
+    pattern1 = r"([yY])(ou)([\W][" + string.ascii_letters + "]*)"
+    match = re.match(pattern1, word)
 
-    if m:
-        g = m.groups()
-        # print(g)
-       
-        if g[0] and g[1]:
-            return f"{g[0]}'all"+ (g[2] if g[2] else '')
+    if match:
+        groups = match.groups()
+
+        if groups[0] and groups[1]:
+            return f"{groups[0]}'all" + (groups[2] if groups[2] else '')
 
     pattern3 = "([yY])(ou)$"
-    m = re.match(pattern3, word)
-    if m:
-        g = m.groups()
-        return f"{g[0]}'all"
+    match = re.match(pattern3, word)
+    if match:
+        groups = match.groups()
+        return f"{groups[0]}'all"
 
-    pattern2 = "(["+string.ascii_letters+"]*)(ing)(\W)?"
+    pattern2 = "([" + string.ascii_letters + r"]*)(ing)(\W)?"
 
-    m = re.match(pattern2, word)
-    if m:
-        g = m.groups()
-        # print(g)
+    match = re.match(pattern2, word)
+    if match:
+        groups = match.groups()
         vowels = 'aeuoiAEUOI'
-        if len(list(filter(lambda e: e in vowels, g[0]))) > 0:
-            return ''.join(g[0])+"in'" + (g[2] if g[2] else '')
+        if len(list(filter(lambda e: e in vowels, groups[0]))) > 0:
+            return ''.join(groups[0]) + "in'" + (groups[2] if groups[2] else '')
 
     return word
 
+
 def test_fry():
+    """ Test the fry function """
     assert fry('you') == "y'all"
     assert fry('You') == "Y'all"
     assert fry('fishing') == "fishin'"
     assert fry('Aching') == "Achin'"
     assert fry('swing') == "swing"
+
+
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
@@ -78,15 +78,9 @@ def main():
     args = get_args()
     text = args.text
 
-    for line in args.text.splitlines():
-        # print(line)
-        # print(re.split('\s', line))
-        print(' '.join([fry(w) for w in re.split('\s', line)]))
-        
+    for line in text.splitlines():
+        print(' '.join([fry(w) for w in re.split(r'\s', line)]))
 
-
-
-    # print(fry(text))
 
 # --------------------------------------------------
 if __name__ == '__main__':
