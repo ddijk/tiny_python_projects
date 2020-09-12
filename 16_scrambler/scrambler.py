ff05df0c56b12ebac37b5e0a0356ccd2d3b88215
ddijk <dick@dijkrosoft.nl>
2020-09-12 16:07:22 +0200

    Implement Scrambler

---
+++ 16_scrambler/scrambler.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""
+Author : dickdijk <dickdijk@localhost>
+Date   : 2020-09-03
+Purpose: Rock the Casbah
+"""
+
+import argparse
+import os
+import re
+import random
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Scramble the letters of words',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text', metavar='text', help='Input text or file')
+
+    parser.add_argument('-s',
+                        '--seed',
+                        help='Random seed',
+                        metavar='seed',
+                        type=int,
+                        default=None)
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.text):
+        args.text = open(args.text).read().strip()
+
+    return args
+
+def scramble(word):
+        """Scramble a word"""
+        if len(word) < 4:
+            return word
+
+
+        middle = [w for w in word[1:-1]]
+        random.shuffle(middle)
+        return word[0]+ ''.join(middle) + word[-1]
+
+
+def test_scramble():
+    """Test scramble"""
+    state = random.getstate()
+    random.seed(1)
+    assert scramble("a") == "a"
+    assert scramble("ab") == "ab"
+    assert scramble("abc") == "abc"
+    assert scramble("abcd") == "acbd"
+    assert scramble("abcde") == "acbde"
+    assert scramble("abcdef") == "aecbdf"
+    assert scramble("abcde'f") == "abcd'ef"
+    random.setstate(state)
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    random.seed(args.seed)
+    for line in args.text.splitlines():
+        words = re.split("([a-zA-Z](?:[a-zA-Z']*[a-zA-Z])?)", line)
+        print(''.join(list(map(scramble, words))))
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
