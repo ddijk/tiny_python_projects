ee063a3b5bd4d536c75d2a055f94a88c3ecd8a9c
ddijk <dick@dijkrosoft.nl>
2020-09-13 21:02:14 +0200

    first implementation

---
+++ 17_mad_libs/mad.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+Author : dickdijk <dickdijk@localhost>
+Date   : 2020-09-12
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+import re
+from pprint import pprint
+import sys
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Mad libs',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('positional',
+                        help='Input file',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'),
+                        default=None)
+
+    parser.add_argument('-i',
+                        '--inputs',
+                        help='Inputs (for testing)',
+                        metavar='input',
+                        type=str,
+                        nargs="*",
+                        default=0)
+
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    inputs = args.inputs
+    pos_arg = args.positional
+
+    # print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
+    # print(f'positional = "{pos_arg.name}"')
+    # print(f'inputs = "{inputs}"')
+
+    # text ='The quick <adjective> <noun> jumps <preposition> the lazy <noun>.'
+
+    text = pos_arg.read().rstrip()
+
+    # print(text)
+
+    match = re.findall('(<([^<>]+)>)', text)
+
+    if len(match) == 0:
+        print(f'"{pos_arg.name}" has no placeholders.', file=sys.stderr)
+        sys.exit(1)
+
+    # pprint(match)
+
+    # print('text before:'+text)
+    inputs.reverse()
+    for a, name in match:
+        val = inputs.pop()
+        text = re.sub('<[^<>]+>', val, text, 1)
+
+    print(text)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
